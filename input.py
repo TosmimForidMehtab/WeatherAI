2ea4f498252aae639076e465bfbd3682563bb6dd
FaizanHashmi3 <mdfaizanhashmi3@gmail.com>
2023-09-13 23:31:13 +0530

    input.py updated

--- input.py
+++ input.py
@@ -2,6 +2,7 @@ import pyttsx3 as tts
 import audioToText as at
 import sys
 import random
+import checkinWeather as ciw
 
 greeting = ['Hey, how can I help you?', 'hello, What can I do for you?', 'Hi, What can I help you with?', 'Hey What can I do for you?', 'Hello mate, How can I help you?']
 
@@ -25,8 +26,35 @@ else:
 if 'vortex' in wake:
     engine.say(greeting[index])
     engine.runAndWait()
-    reply1 = at.speechToText()
-    print(reply1) # This is printing none
+    
+    # print(reply1)  This is printing none
+   
+    flag=True
+    while(True):
+        if flag:
+             reply1 = at.speechToText()
+            
+       
+        if 'weather' in reply1:
+            engine.say(questions1[index])
+            engine.runAndWait()
+            reply2=at.speechToText() #asking for city
+            reply2Array=reply2.split() #checking for last word in string array
+            city=reply2Array[-1]
+            engine.say(ciw.getWeather(city) )#calling weather function
+            engine.runAndWait()
+            
+            ####checking for exit or if user want to use it again 
+            reply3=at.speechToText()#checking for exit/stop word or "weather"word to continue this
+            if 'exit' in reply3 or 'stop' in reply3:
+                break
+            elif 'weather' in reply3: #if user say "i want to know the weather of other place"
+                flag=False #so that reply1 will not be taken again
+            else:
+                engine.say("sorry, i am unable to understand, say it again")   
+        else:
+            engine.say("sorry, i am unable to understand, say it again")
+    
 else:
     engine.say('Sorry, I did not understand')
         
